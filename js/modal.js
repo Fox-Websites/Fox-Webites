da08fb1f243940edc9a592c1511d584f0debdaf2
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-10-21 00:47:36 -0300

    Criado modal para o Orçamento

---
+++ js/modal.js
@@ -0,0 +1,23 @@
+const btnOrcamentoNav = document.getElementById("header__menu__orcamento");
+const btnBanner = document.getElementById("banner__cta");
+const btnOrcamentoBudget = document.getElementById("budget__content__cta");
+
+const modalContainer = document.getElementById("modal_container");
+const btnClose = document.getElementById("modal_close");
+
+function openModal() {
+  modalContainer.classList.add("show");
+}
+
+btnOrcamentoNav.addEventListener("click", openModal);
+if (btnBanner !== null) {
+  btnBanner.addEventListener("click", openModal);
+}
+
+if (btnOrcamentoBudget !== null) {
+  btnOrcamentoBudget.addEventListener("click", openModal);
+}
+
+btnClose.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
---
+++ js/scrollReveal.js
@@ -0,0 +1,19 @@
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: "1000",
+});
+
+// HOME
+ScrollReveal().reveal("#schedule__title", { delay: 200 });
+ScrollReveal().reveal("#schedule__subtitle", { delay: 250 });
+ScrollReveal().reveal(".schedule__item", { delay: 300 });
+
+ScrollReveal().reveal("#differential__title", { delay: 200 });
+ScrollReveal().reveal("#differential__subtitle", { delay: 250 });
+ScrollReveal().reveal(".differential__item", { delay: 300 });
+
+ScrollReveal().reveal("#projects__title", { delay: 200 });
+ScrollReveal().reveal("#projects__subtitle", { delay: 250 });
+ScrollReveal().reveal(".project__item", { delay: 300 });
+ScrollReveal().reveal("#projects__button", { delay: 350 });
